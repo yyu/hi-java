7c1eb5634261881ef4cf1c4db6ce79a9cd03629c
y² <yy@yuyuan.org>
2022-01-30 19:10:00 -0800

    add subaru

---
+++ cmdline/src/org/yuyuan/Subaru.java
@@ -0,0 +1,13 @@
+package org.yuyuan;
+
+public class Subaru implements Car {
+    @Override
+    public int getMaxSpeed() {
+        return 0;
+    }
+
+    @Override
+    public String getColor() {
+        return "Green";
+    }
+}
